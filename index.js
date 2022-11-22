c650d3deef67a34ed297c8a5d12b6cb2588b0d64
Freddie16 <freddiemurigi@gmail.com>
2022-11-22 10:31:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    let canthear = "I can't hear you!";
+    let canhear = "YES INDEED!";
+    let loveto = "I would love to!";
+    if(string.toLowerCase(string)===string) {
+        return canthear
+    }
+
+    else if(string.toUpperCase(string)===string) {
+        return canhear
+    }
+
+    else if("Let's have dinner together!"===string){
+        return loveto
+    }
+} 
+    
+    
